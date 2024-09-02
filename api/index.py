02c403b485c431aa25d45c0597eaea39be67a198
Mithun Rajkumar <148180154+vaincleric@users.noreply.github.com>
2024-09-02 14:03:18 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,18 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+class Minesweeper:
+    def print_board(self):
+        return "Sample Board"
+
 class handler(BaseHTTPRequestHandler):
+    game = Minesweeper()
+
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        response = self.game.print_board()
+        self.wfile.write(response.encode('utf-8'))
 
 def run(server_class=HTTPServer, handler_class=handler, port=8080):
     server_address = ('', port)
@@ -15,4 +22,3 @@ def run(server_class=HTTPServer, handler_class=handler, port=8080):
 
 if __name__ == "__main__":
     run()
-
